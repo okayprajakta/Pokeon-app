7b6fe22684ad3e17c71b5bcc4cd28a70c6536dd1
Prajakta <prajaktasurvase.nbnscoe.entc@gmail.com>
2025-01-08 17:05:44 +0530

    Pokemon-authentication done.

--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
-from src.routers import pokemon_router  # Ensure correct import
-from src.models.pokemon_model import Base
-from src.config.database import engine
-from src.data.load_data import load_pokemon_data
-from src.config.database import SessionLocal
+from src.routers import pokemon_router, auth_router
+# from src.models.pokemon_model import Base as PokemonBase
+# from src.models.user_model import Base as UserBase
+# from src.config.database import engine
+
 
 app = FastAPI()
 
-# Create all tables in the database
-Base.metadata.create_all(bind=engine)
+# # Create all tables in the database
+# PokemonBase.metadata.create_all(bind=engine)
+# UserBase.metadata.create_all(bind=engine)
 
-# Load Pokémon data into the database
-load_pokemon_data(db=SessionLocal())
-
-# Include the Pokémon router
+# Include authentication and Pokémon routers
+app.include_router(auth_router.router, tags=["Authentication"])
 app.include_router(pokemon_router.router, tags=["Pokémon"])
+
